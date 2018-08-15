332110e5f1ecfbca772829a9f182069ee374c414
Juan David Castro <me@juandc.co>
2018-08-14 21:13:19 -0500

    API Working

--- src/api.js
+++ src/api.js
@@ -4,36 +4,50 @@ import AWS from 'aws-sdk';
 
 const request = require('request').defaults({ encoding: null })
 
+const app = express();
 const rek = new AWS.Rekognition({
   region: 'us-west-2',
-  accessKeyId: "hmmmm",
-  secretAccessKey: "boooo",
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
-const app = express();
 
-const TEST_IMAGE = 'https://s3.us-east-2.amazonaws.com/serverless-rekognition-platzi/upload/IMG_20171119_104715_250.jpeg';
-const TEST_BUCKET = 'ohh';
-const TEST_NAME = 'ayayay';
+console.log(process.env)
 
-function Detectlabels(req, res) {
-  request.get(TEST_IMAGE, (err, _, body) => {
-    const params = {
-      Image: {
-        Bytes: body,
-      },
-      MaxLabels: 20,
-      MinConfidence: 80,
+app.get('/', (req, res) => {
+  GetImageLabels(req.query, (err, data) => {
+    if (err) return res.json({ message: err.message, error: true });
+
+    res.json({
+      image: req.query.img || req.query.image,
+      message: 'Success!',
+      data,
+    });
+  });
+});
+
+function GetImageLabels(params, func) {
+  params = {
+    img: params.img || params.image || func(new Error('The image param cannot be null.')),
+    maxLabels: params.maxLabels || params.MaxLabels || 10,
+    minConfidence: params.minConfidence || params.MinConfidence || 80,
+  };
+
+  if (params.img.length <= 8) return func(new Error('The image param length can not be <= 8.'));
+
+  request.get(params.img, (reqErr, _, body) => {
+    if (reqErr) return func(new Error('Error in request: ' + reqErr));
+
+    const rekParams = {
+      Image: { Bytes: body },
+      MaxLabels: params.maxLabels,
+      MinConfidence: params.minConfidence,
     };
 
-    rek.detectLabels(params, (err, data) => {
-      if (err) return console.log(err);
-      console.log(data)
+    rek.detectLabels(rekParams, (rekErr, data) => {
+      if (rekErr) return func(new Error('Error in rek: ' + rekErr));
+      return func(null, data);
     });
-  })
-
-  res.send('Todo bien compa');
+  });
 }
-
-app.get('/api/:id', Detectlabels)
 
 export default serverless(app);
