43890fc48f506a3b4e712d3143f590bca836a8df
Juan David Castro <me@juandc.co>
2018-08-15 10:46:29 -0500

    Refactor and Deploy

--- src/api.js
+++ src/api.js
@@ -1,17 +1,15 @@
 import serverless from 'serverless-http';
 import express from 'express';
 import AWS from 'aws-sdk';
-
-const request = require('request').defaults({ encoding: null })
+import request from 'request';
 
 const app = express();
+const requests = request.defaults({ encoding: null });
 const rek = new AWS.Rekognition({
   region: 'us-west-2',
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
-
-console.log(process.env)
 
 app.get('/', (req, res) => {
   GetImageLabels(req.query, (err, data) => {
@@ -27,14 +25,15 @@ app.get('/', (req, res) => {
 
 function GetImageLabels(params, func) {
   params = {
-    img: params.img || params.image || func(new Error('The image param cannot be null.')),
+    img: params.img || params.image || null,
     maxLabels: params.maxLabels || params.MaxLabels || 10,
     minConfidence: params.minConfidence || params.MinConfidence || 80,
   };
 
+  if (!params.img) return func(new Error('The image param cannot be null.'));
   if (params.img.length <= 8) return func(new Error('The image param length can not be <= 8.'));
 
-  request.get(params.img, (reqErr, _, body) => {
+  requests.get(params.img, (reqErr, _, body) => {
     if (reqErr) return func(new Error('Error in request: ' + reqErr));
 
     const rekParams = {
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,3 @@
-// const path = require('path');
-// const slsw = require('serverless-webpack');
-// const nodeExternals = require('webpack-node-externals');
-
-// module.exports = {
-//   entry: slsw.lib.entries,
-//   // entry: './src/api.js',
-//   target: 'node',
-//   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-//   optimization: {
-//     // We no not want to minimize our code.
-//     minimize: false
-//   },
-//   performance: {
-//     // Turn off size warnings for entry points
-//     hints: false
-//   },
-//   devtool: 'nosources-source-map',
-//   externals: [nodeExternals()],
-//   output: {
-//     libraryTarget: 'commonjs2',
-//     path: path.join(__dirname, '.webpack/src'),
-//     filename: 'api.js'
-//   }
-// };
-
 const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
