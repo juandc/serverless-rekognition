b6f964c9d934279dc167c48d4fe796310f51f14f
Juan David Castro <me@juandc.co>
2018-08-14 20:34:03 -0500

    Image labels working with url-images

--- src/api.js
+++ src/api.js
@@ -1,22 +1,39 @@
 import serverless from 'serverless-http';
 import express from 'express';
+import AWS from 'aws-sdk';
 
-const app = express()
+const request = require('request').defaults({ encoding: null })
 
-app.get('/', (req, res) => {
-  res.send('Hello! <a href="./api">Test an Image!</a>')
-})
-
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'API',
-  })
+const rek = new AWS.Rekognition({
+  region: 'us-west-2',
+  accessKeyId: "hmmmm",
+  secretAccessKey: "boooo",
 });
+const app = express();
 
-app.get('/api/:id', (req, res) => {
-  res.json({
-    message: `API for ${req.params.id}`,
+const TEST_IMAGE = 'https://s3.us-east-2.amazonaws.com/serverless-rekognition-platzi/upload/IMG_20171119_104715_250.jpeg';
+const TEST_BUCKET = 'ohh';
+const TEST_NAME = 'ayayay';
+
+function Detectlabels(req, res) {
+  request.get(TEST_IMAGE, (err, _, body) => {
+    const params = {
+      Image: {
+        Bytes: body,
+      },
+      MaxLabels: 20,
+      MinConfidence: 80,
+    };
+
+    rek.detectLabels(params, (err, data) => {
+      if (err) return console.log(err);
+      console.log(data)
+    });
   })
-});
+
+  res.send('Todo bien compa');
+}
+
+app.get('/api/:id', Detectlabels)
 
 export default serverless(app);
